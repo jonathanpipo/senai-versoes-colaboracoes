d477ea4b20b2bd60b5a0f871dfb8b549768e1ce0
Jonathan Pipo <89938164+jonathanpipo@users.noreply.github.com>
2021-09-28 21:32:36 -0300

    Atividade presencial 2

---
+++ index.js
@@ -0,0 +1,11 @@
+const aluno = 10
+
+for(let cont = 0; cont <= aluno; cont++)
+
+if (cont %2 == 0) {
+    console.log('é par');
+}else if(cont %2 != 0){
+    console.log('é impar');
+}else {
+    console.log('é zero')
+}
